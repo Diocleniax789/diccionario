1e0a0d64df9ae3a1563094682e4b394f9d05a31d
Diocleniax789 <julioramdom123@gmail.com>
2024-12-06 17:51:07 -0300

    se crearon los prototipos y tambien el tipo de dato struct

--- diccionario.c
+++ diccionario.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
+struct palabras{
+    char pal[20];
+    int pos;
+};
+
+char * carga(int*);
+struct palabras * separa(char*,int,int*);
+int diccionario(char[500][20]);
+struct palabras * corrector(struct palabras*,int,char dic[500][20],int);
+
 int main(){
 
 
